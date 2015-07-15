b0108c0181890b4c166e903bcdbda79e267f091e
Jon Zelner <jzelner@gmail.com>
2015-07-15 11:21:12 -0400

    updated makefile to create point data and updated point data file to generate both pairs of events and individual points. Still need to chop off lagged days from individual points

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: data
 
-data : cache/crime/police_inct_clean.csv 
+data : cache/crime/police_inct_clean.csv
 
 ###################################################################################
-## PROCESSING OF RAW DATA INTO CLEANED AND AGGREGATED DATA
+## PROCESSING OF RAW DATA INTO CLEANED DATA
 
 ## Rasterize neighborhood-level data
 cache/spatial/phl_raster.grd : munge/rasterize.R data/spatial/Neighborhoods_Philadelphia.geojson
@@ -16,14 +16,14 @@ cache/spatial/raster_adjacency.csv : munge/adjacency.R cache/spatial/phl_raster.
 	./$< -d $(word 2, $^) -o $@
 
 ## Get a clean set of crime categories and data only from 1/1/2006-12/31/2014
-## and corresponding raster squares 
+## and corresponding raster squares
 cache/crime/police_inct_clean.csv : munge/clean_crime_data.R data/crime/police_inct.csv cache/spatial/phl_raster.grd
 	$(info **** CLEANING RAW CRIME DATA ***)
 	./$< -d $(word 2, $^) -o $@ -r $(word 3, $^)
 
-cache/crime/daily_crime_counts.csv : munge/generate_daily_crime_counts.R cache/crime/police_inct_clean.csv
-	$(info **** PROCESSING RAW CRIME DATA INTO CITY-WIDE DAILY COUNTS ****)
-	./$< -d $(word 2, $^) -o $@
+
+###################################################################################
+## DESCRIPTIVES
 
 ## Make a raster stack with counts for every crime type by location
 cache/crime_stack.grd : munge/stack.R cache/spatial/phl_raster.grd cache/crime/police_inct_clean.csv
@@ -31,21 +31,28 @@ cache/crime_stack.grd : munge/stack.R cache/spatial/phl_raster.grd cache/crime/p
 	./$< -s $@ -r $(word 2, $^) -d $(word 3, $^)
 
 
+cache/crime/daily_crime_counts.csv : munge/generate_daily_crime_counts.R cache/crime/police_inct_clean.csv
+	$(info **** PROCESSING RAW CRIME DATA INTO CITY-WIDE DAILY COUNTS ****)
+	./$< -d $(word 2, $^) -o $@
+
+
 ###################################################################################
 ## MAKING FIGURES
+
 graphs/counts/*.pdf : src/weekly_counts.R cache/crime/daily_crime_counts.csv
 	./$<
 
-################################################################################
+################################################################################oop
 ## MAKING MOVIE
 output/movie/movie.mp4 : src/movie/movie.R cache/crime/police_inct_clean.csv
 	./$< -d $(word 2, $^) -o $@
 
 ################################################################################
-## Make input dataset for point-process model
+## POINT PROCESS MODEL
+
+## PROCESS DATA FOR INPUT TO MODEL
 max_d := 2
 max_t := 60
-cache/point_data.csv : munge/point_data.R cache/crime/police_inct_clean.csv
-	./$< -d $(word 2, $^) -o $@ -t $(max_t) -x $(max_d)
-
-
+ppmdata: cache/input_pairs.csv cache/input_points.csv
+cache/input_pairs%csv cache/input_points%csv : munge/point_data.R cache/crime/police_inct_clean.csv
+	./$< -d $(word 2, $^) -o cache/input_pairs$*csv -p cache/input_points$*csv -t $(max_t) -x $(max_d)
--- munge/point_data.R
+++ munge/point_data.R
@@ -1,11 +1,12 @@
-#!/usr/bin/Rscript
+#!/Usr/bin/Rscript
 require(docopt)
 'Usage:
-   point_data.R [-d <data> -o <output>]
+   point_data.R [-d <data> -o <output> -p <points> -x <distance> -t <time> ]
 
 Options:
    -d Input data [default: cache/crime/police_inct_clean.csv]
-   -o Output data [default: cache/point_data.csv]
+   -p Output file for pairs [default: cache/input_pairs.csv]
+   -o Output file for points [default: cache/input_points.csv]
    -t Maximum time between related cases (days) [default: 60]
    -x Maximum distance between related cases (km) [default: 2]
 ' -> doc
@@ -24,14 +25,14 @@ max_d <- as.numeric(opts$x)
 ## Load up point data and filter out any points with NAs
 d <- read_csv(opts$d)
 
-d %>>% filter(TEXT_GENERAL_CODE %in% c("Homicide - Criminal", "Aggravated Assault Firearm")) %>>%
+d %>>% filter(TEXT_GENERAL_CODE %in% c("Homicide - Criminal")) %>>%
 filter(!is.na(POINT_X) == TRUE) %>>%
 {
     .$DISPATCH_DATE <- ymd(.$DISPATCH_DATE)
     return(.)
 } %>>%
 arrange(DISPATCH_DATE) %>>%
-select(POINT_X, POINT_Y, DISPATCH_DATE, TEXT_GENERAL_CODE) %>>%
+select(UTM_X, UTM_Y, DISPATCH_DATE, TEXT_GENERAL_CODE, RASTER_ID, RASTER_CELL) %>>%
 ##Add week and year
 {mutate(.,week = week(DISPATCH_DATE),
         year = year(DISPATCH_DATE),
@@ -41,40 +42,65 @@ select(POINT_X, POINT_Y, DISPATCH_DATE, TEXT_GENERAL_CODE) %>>%
 crime_d$days <- rep(0, nrow(crime_d))
 
 for (i in 1:nrow(crime_d)) {
-    crime_d$days[i] <- as.numeric(crime_d$DISPATCH_DATE[i]-min(crime_d$DISPATCH_DATE)) +1
-    
+    crime_d$days[i] <- as.numeric(crime_d$DISPATCH_DATE[i]-min(crime_d$DISPATCH_DATE)) +1   
 }
-## For each crime, get the set of crimes with days less than this crime
 
+## Make a sequence of weeks from the first to the last
+all_weeks <- seq(from=min(crime_d$DISPATCH_DATE), to = max(crime_d$DISPATCH_DATE), by = "week")
+week_df <- data.frame(total_weeks = 1:length(all_weeks),
+                      week = week(all_weeks),
+                      year = year(all_weeks))
+
+## Merge into crime data
+crime_d <- merge(crime_d, week_df, by = c("week", "year"))
+## For each crime, get the set of crimes with days less than this crime
 ## First, get unique days in dataset
 days <- unique(crime_d$days)
 
 ##Get the first day outside the lag length
+### TODO: CHOP OFF FIRST X ROWS OF POINT DATA AND BE SURE
+### TO RE-NORMALIZE DAYS/WEEKS SO THAT THE NORMALIZED DATA
+### START AT ONE
 first_index <- which(days > max_t)[1]
 
 out_df <- data.frame()
-for (i in first_index:length(days)) {
-    d <- days[i]
-    ## First filter out the ones corresponding to this day
-    current_d <- filter(crime_d, days == d)
-    lag_d <- filter(crime_d, days < d, days > d-max_t)
 
-    print(i)
-    print(nrow(current_d))
-    print(nrow(lag_d))
+from <- c()
+to <- c()
+lags <- c()
+distance <- c()
+for (i in first_index:length(days)) {
+    cday <- days[i]
+    ## First filter out the ones corresponding to this day
+    current_d <- filter(crime_d, days == cday)
+    lag_d <- filter(crime_d, days < cday, days > cday-max_t)
 
     ## Lag is uniform for all day/
-    time_lag <- d - lag_d$days
-    lag_string <- paste(time_lag, collapse=",")
-    current_d$time_lags <- lag_string
 
     current_d$distance <- rep(0, nrow(current_d))
     for (j in 1:nrow(current_d)) {
-        distance <- sqrt((current_d$POINT_X[j] - lag_d$POINT_X)**2 + (current_d$POINT_Y[j] - lag_d$POINT_Y)**2)
-        print(j)
-        current_d$distance[j] <- paste(distance[distance <= max_d],collapse=",")
-    }
-    out_df <- rbind(out_df, current_d)
-}
+        lag_d$distance <- sqrt(((current_d$UTM_X[j] - lag_d$UTM_X)**2) + ((current_d$UTM_Y[j] - lag_d$UTM_Y)**2))/1000
 
-write_csv(out_df, opts$o)
+        close_lag_d <- filter(lag_d, distance <= max_d)
+        time_lag <- cday - close_lag_d$days
+
+
+        from <- append(from, close_lag_d$id)
+        to <- append(to, rep(current_d$id[j], nrow(close_lag_d)))
+        lags <- append(lags, time_lag)
+        distance <- append(distance, close_lag_d$distance)
+        #current_d$distance[j] <- paste(distance[distance <= max_d],collapse=",")
+    }
+
+}
+    out_df <- data.frame(from = from,
+                         to = to,
+                         distance = distance,
+                         lag = lags)
+
+## Write out pairs
+write_csv(out_df, opts$p)
+names(crime_d) <- toupper(names(crime_d))
+
+## Write out points
+write_csv(crime_d, opts$o)
